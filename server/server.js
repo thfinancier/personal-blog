5b693f92aec76ea81debd63937ac4a0f1f6289fd
The Financier <itisenward@gmail.com>
2023-07-18 19:31:23 +0600

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`Server is running on port: ${port}`))
