c5e82fe089b47ebf1f3679786f3b916fdafc6566
The Financier <itisenward@gmail.com>
2023-07-25 22:44:57 +0600

    register user route added

---
+++ server/controllers/userController.js
@@ -0,0 +1,8 @@
+const registerUser = async (req, res) => {
+    res.send('Register user')
+}
+
+
+module.exports = {
+    registerUser,
+}
--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/posts', require('./routes/postRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
